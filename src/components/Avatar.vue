bec44a2f2e899b4223841bfa95ae44b6baff7987
Emir <emrggck@gmail.com>
2020-09-08 21:09:46 +0300

    First commit
    Proje bitti

---
+++ src/components/Avatar.vue
@@ -0,0 +1,42 @@
+<template>
+  <figure class="avatar">
+    <img :src="src" :alt="alt" :width="size" :height="size" />
+  </figure>
+</template>
+
+<script>
+export default {
+  name: 'Avatar',
+  props: {
+    src: {
+      type: String,
+      default:
+        'https://instagram.fada2-1.fna.fbcdn.net/v/t51.2885-19/s150x150/82051726_182131399700831_2537802295811244032_n.jpg?_nc_ht=instagram.fada2-1.fna.fbcdn.net&_nc_ohc=pSif1OA1DrAAX-ll4np&oh=46c883c56a69e560083421dfcd86fc5a&oe=5F81D106'
+    },
+    alt: {
+      type: String
+    },
+    size: {
+      type: Number
+    }
+  }
+}
+</script>
+
+<style scoped>
+.avatar {
+  display: inline-flex;
+  padding: 2px;
+  border-radius: 999px;
+  background: linear-gradient(
+    219deg,
+    rgba(184, 63, 140, 1) 0%,
+    rgba(237, 158, 90, 1) 100%
+  );
+
+  img {
+    border-radius: inherit;
+    border: 2px solid white;
+  }
+}
+</style>
---
+++ src/components/Post.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="post">
+    <!-- header -->
+    <header class="header section">
+      <div class="header-author">
+        <Avatar :size="40" />
+        <div class="header-author-info">
+          <div>
+            <CustomText tag="b">emirgocukk</CustomText>
+          </div>
+          <div>
+            <CustomText size="xsmall">Adana, Çukurova</CustomText>
+          </div>
+        </div>
+      </div>
+      <div class="header-more">
+        <button type="button">
+          <IconMore />
+        </button>
+      </div>
+    </header>
+
+    <!-- media -->
+    <div class="post-media">
+      <img
+        src="https://instagram.fada2-1.fna.fbcdn.net/v/t51.2885-15/e35/119007810_640248006885980_5834262255091108688_n.jpg?_nc_ht=instagram.fada2-1.fna.fbcdn.net&_nc_cat=1&_nc_ohc=Jqr1cfaP804AX_SqM12&oh=dc873cad3f1b14b118777824ded5467d&oe=5F804F47"
+        alt=""
+      />
+    </div>
+
+    <!-- actions -->
+    <div class="action-buttons section">
+      <button type="button">
+        <IconLike />
+      </button>
+      <button type="button">
+        <IconComment />
+      </button>
+      <button type="button">
+        <IconDirect />
+      </button>
+      <button class="action-save" type="button">
+        <IconSave />
+      </button>
+    </div>
+
+    <!-- view count -->
+
+    <div class="view-count section">
+      <custom-text tag="b">110 beğenme</custom-text>
+    </div>
+
+    <!-- comment list -->
+
+    <!-- datetime -->
+
+    <!-- comment form -->
+  </div>
+</template>
+
+<script>
+import Avatar from '@/components/Avatar'
+import CustomText from '@/components/CustomText'
+import IconMore from '@/icons/more.svg'
+import IconLike from '@/icons/like.svg'
+import IconComment from '@/icons/comment.svg'
+import IconDirect from '@/icons/direct.svg'
+import IconSave from '@/icons/save.svg'
+
+export default {
+  name: 'Post',
+  components: {
+    IconLike,
+    IconComment,
+    IconDirect,
+    IconSave,
+    IconMore,
+    CustomText,
+    Avatar
+  }
+}
+</script>
+
+<style scoped>
+.post {
+  border-radius: 3px;
+  border: 1px solid rgb(var(--b6a));
+}
+
+.section {
+  padding-left: 16px;
+  padding-right: 16px;
+}
+
+.header {
+  height: 60px;
+  display: flex;
+  align-items: center;
+
+  &-author {
+    display: flex;
+    align-items: center;
+
+    &-info {
+      margin-left: 8px;
+    }
+  }
+
+  &-more {
+    margin-left: auto;
+  }
+}
+
+.action-buttons {
+  height: 45px;
+  display: flex;
+  align-items: center;
+
+  button {
+    margin-right: 16px;
+  }
+
+  .action-save {
+    margin-left: auto;
+    margin-right: 0;
+  }
+}
+</style>
